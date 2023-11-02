a4a552ee8d195ab3ed9a865355b1b8536903dd0f
Tatana Dudackova <tanuldab@gmail.com>
2023-11-02 19:48:11 +0100

    prvni pokus o propojeni obou tabulek

--- sql_project_dudackova.sql
+++ sql_project_dudackova.sql
@@ -320,3 +320,23 @@ END AS vyvoj_mzdy
 FROM czechia_payroll cp
 WHERE cp.industry_branch_code IS NOT NULL AND cp.value_type_code ='5958'
 ORDER BY id; -- funguje TO, ale jeste zkusit nejak ucesat a overit, ze tam nejsou blbosti
+-- --------------------------------------------------------------------------------------------------------
+NOVY POKUS - propojeni tabulek
+
+SELECT *
+FROM czechia_payroll cp
+
+
+SELECT *
+FROM czechia_price cp;
+
+SELECT cp.id,cp.value,cp.category_code,YEAR(cp.date_from),region_code
+FROM czechia_price cp
+ORDER BY year(cp.date_from);
+
+SELECT *
+FROM czechia_payroll cp 
+LEFT JOIN czechia_price cp2
+ON cp.payroll_year = YEAR(cp2.date_from)
+WHERE cp.industry_branch_code IS NOT NULL AND cp.value_type_code ='5958'
+ORDER BY payroll_year;
