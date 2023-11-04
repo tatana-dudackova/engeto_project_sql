599c1d2635defe4b5dd33d3deee71bfb3659ca46
Tatana Dudackova <tanuldab@gmail.com>
2023-11-04 12:41:57 +0100

    kosmeticka zmena

--- question_1.sql
+++ question_1.sql
@@ -4,4 +4,4 @@ FROM t_tatana_dudackova_project_sql_primary_final;
 
 SELECT cp_id, payroll_value,industry_branch_code,payroll_year,payroll_quarter 
 FROM t_tatana_dudackova_project_sql_primary_final
-ORDER BY payroll_year; -- poznamka - zkusit prijoinovat znova rok a ctvrtleti
+ORDER BY payroll_year; -- poznamka - zkusit prijoinovat znova rok a ctvrtleti 
