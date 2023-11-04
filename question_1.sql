54da111265ab8a23633bff570b185d4f2de5d6d2
Tatana Dudackova <tanuldab@gmail.com>
2023-11-04 13:31:15 +0100

    drobna zmena

--- question_1.sql
+++ question_1.sql
@@ -1,9 +1,10 @@
 
 SELECT * 
-FROM t_tatana_dudackova_project_sql_primary_final;
+FROM t_tatana_dudackova_project_sql_primary_final
+ORDER BY industry_branch_code, payroll_year,payroll_quarter;
 
 SELECT ttdpspf.payroll_value,ttdpspf2.payroll_value AS payroll_value_next_year, ttdpspf.industry_branch_code,ttdpspf.payroll_year,ttdpspf2.payroll_year AS next_year,ttdpspf.payroll_quarter 
 FROM t_tatana_dudackova_project_sql_primary_final ttdpspf
-JOIN t_tatana_dudackova_project_sql_primary_final ttdpspf2
-ON ttdpspf.cp_id = ttdpspf2.cp_id -- joinuju obe tabulky pres id za podminky viz nize, pro jistotu pridavam podminku rovnosti u dalsich sloupcu
+LEFT JOIN t_tatana_dudackova_project_sql_primary_final ttdpspf2
+ON ttdpspf.cp_id = ttdpspf2.cp_id -- joinuju obe tabulky pres id za podminky viz nize, mohla bych pro jistotu pridat podminku rovnosti dalsich sloupcu, ale uz ted se mi prikaz vykonava priserne pomalu
 WHERE ttdpspf.payroll_year = ttdpspf2.payroll_year + 1;
