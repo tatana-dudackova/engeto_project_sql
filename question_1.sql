c00f24cfcf59cb70407cc8e98cc3911bc0d77b2e
Tatana Dudackova <tanuldab@gmail.com>
2023-11-04 13:20:24 +0100

    pokus o propojeni tabulek, ale prikaz se vykonava strasne pomalu :(

--- question_1.sql
+++ question_1.sql
@@ -2,6 +2,8 @@
 SELECT * 
 FROM t_tatana_dudackova_project_sql_primary_final;
 
-SELECT cp_id, payroll_value,industry_branch_code,payroll_year,payroll_quarter 
-FROM t_tatana_dudackova_project_sql_primary_final
-ORDER BY payroll_year;   -- poznamka - zkusit prijoinovat znova rok a ctvrtleti 
+SELECT ttdpspf.payroll_value,ttdpspf2.payroll_value AS payroll_value_next_year, ttdpspf.industry_branch_code,ttdpspf.payroll_year,ttdpspf2.payroll_year AS next_year,ttdpspf.payroll_quarter 
+FROM t_tatana_dudackova_project_sql_primary_final ttdpspf
+JOIN t_tatana_dudackova_project_sql_primary_final ttdpspf2
+ON ttdpspf.cp_id = ttdpspf2.cp_id -- joinuju obe tabulky pres id za podminky viz nize, pro jistotu pridavam podminku rovnosti u dalsich sloupcu
+WHERE ttdpspf.payroll_year = ttdpspf2.payroll_year + 1;
