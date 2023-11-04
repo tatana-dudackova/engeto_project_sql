79ae1e6509595dffbe5fa458f5626eaef882d8ef
Tatana Dudackova <tanuldab@gmail.com>
2023-11-04 11:46:19 +0100

    primarni tabulka

--- primary_table_dudackova.sql
+++ primary_table_dudackova.sql
@@ -7,6 +7,19 @@ JOIN economies e
     and c.independence_date <= e.year
 ;
 
+CREATE TABLE pokus AS (
+SELECT cp.id AS cp_id, cp.value AS cp_value, cp.value_type_code, cp.unit_code, cp.calculation_code,cp.industry_branch_code,cp.payroll_year,cp.payroll_quarter,cp2.id AS cp2_id, cp2.value AS cp2_value, cp2.category_code, cp2.date_from, cp2.date_to,e.GDP  
+FROM czechia_payroll cp 
+LEFT JOIN czechia_price cp2
+ON cp.payroll_year = YEAR(cp2.date_from)
+LEFT JOIN economies e 
+ON cp.payroll_year = e.`year`
+WHERE cp.industry_branch_code IS NOT NULL AND cp.value_type_code ='5958' AND e.country = 'czech republic');
+
+-- poznamky: sloupce, ktere se ve slucovanych tabulkach jmenuji stejne, jsem musela prejmenovat
+-- z tabulky economies jsem pro ceskou republiku vybrala jen sloupec HDP
+-- v popisu k projektu popsat take podminku where, proc jsem to zvolila tak, jak jsem zvolila
+-- z tabulky czechia payroll jsem vyhodila region code, protoze ho vzhledem k zadani urcite nebudu potrebovat
 
 SELECT *
 FROM czechia_payroll cp 
