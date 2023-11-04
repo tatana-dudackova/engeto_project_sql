72209e91279ddc0746ab9898b71241fe6088a0a6
Tatana Dudackova <tanuldab@gmail.com>
2023-11-04 11:48:17 +0100

    pridala jsem jeste poznamku

--- primary_table_dudackova.sql
+++ primary_table_dudackova.sql
@@ -1,12 +1,3 @@
-SELECT c.country , c.capital_city, c.independence_date , e.year, 
-    round( e.GDP / 1000000, 2 ) as GDP_mil_dollars , 
-    e.population , e.gini , e.taxes 
-FROM countries c 
-JOIN economies e 
-    on c.country = e.country 
-    and c.independence_date <= e.year
-;
-
 CREATE TABLE pokus AS (
 SELECT cp.id AS cp_id, cp.value AS cp_value, cp.value_type_code, cp.unit_code, cp.calculation_code,cp.industry_branch_code,cp.payroll_year,cp.payroll_quarter,cp2.id AS cp2_id, cp2.value AS cp2_value, cp2.category_code, cp2.date_from, cp2.date_to,e.GDP  
 FROM czechia_payroll cp 
@@ -20,6 +11,7 @@ WHERE cp.industry_branch_code IS NOT NULL AND cp.value_type_code ='5958' AND e.c
 -- z tabulky economies jsem pro ceskou republiku vybrala jen sloupec HDP
 -- v popisu k projektu popsat take podminku where, proc jsem to zvolila tak, jak jsem zvolila
 -- z tabulky czechia payroll jsem vyhodila region code, protoze ho vzhledem k zadani urcite nebudu potrebovat
+-- tabulku pak vytvorit znovu a pojmenovat ji tak, jak ji mame pojmenovat
 
 SELECT *
 FROM czechia_payroll cp 
@@ -28,3 +20,6 @@ ON cp.payroll_year = YEAR(cp2.date_from)
 LEFT JOIN economies e 
 ON cp.payroll_year = e.`year`
 WHERE cp.industry_branch_code IS NOT NULL AND cp.value_type_code ='5958' AND e.country = 'czech republic';
+
+SELECT * 
+FROM pokus;
