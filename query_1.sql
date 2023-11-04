0b519b07d38ef45e33cab0ef3866934531da0f9e
Tatana Dudackova <tanuldab@gmail.com>
2023-11-04 21:51:36 +0100

    ...

--- query_1.sql
+++ query_1.sql
@@ -39,8 +39,30 @@ AND cp.calculation_code = cp2.calculation_code
 AND cp.industry_branch_code = cp2.industry_branch_code 
 AND cp.payroll_year = cp2.payroll_year +1
 AND cp.payroll_quarter = cp2.payroll_quarter 
-WHERE cp.value_type_code = '5958' AND cp.calculation_code  = '200'
+WHERE cp.value_type_code = '5958' AND cp.calculation_code  = '200' AND cp.unit_code='200' -- pozn.  - jeste jsem pridala ten unit code, aby TO bylo v korunach a ne tisicich osob
 ORDER BY cp.id,cp2.id;
-
 -- prikaz viz vyse zkusit udelat pak podle te finalni tabulky, pokusit se ale mit v te finalni tabulce uz prijoinovany ten spravny sloupec
+-- pozn. - pozor, mam tam chybu :( - od nejakeho radku 1053, nebo tak :( asi kvuli idickum, to pak take napsat do popisu
+-- --------------------
+
+-- pokus o opravu
+
+SELECT DISTINCT cp.id,cp2.id, cp.value,cp2.value AS druha_tabulka_value,cp.industry_branch_code, cp2.industry_branch_code, cp.payroll_year,cp.payroll_quarter,cp2.payroll_year +1 AS payroll_year2,cp2.payroll_quarter AS payroll_quarter2,
+CASE 
+	WHEN cp.value>cp2.value THEN 'mzdy vzrostly'
+	WHEN cp2.value>cp.value THEN 'mzdy klesly'
+	WHEN cp2.value IS NULL THEN 'chybi nam udaje'
+	ELSE 'mzdy zustaly stejne'
+END AS porovnani
+FROM czechia_payroll cp
+LEFT JOIN czechia_payroll cp2 
+ON cp.value_type_code = cp2.value_type_code
+AND cp.unit_code = cp2.unit_code 
+AND cp.calculation_code = cp2.calculation_code
+AND cp.industry_branch_code = cp2.industry_branch_code 
+AND cp.payroll_year = cp2.payroll_year +1
+AND cp.payroll_quarter = cp2.payroll_quarter 
+WHERE cp.value_type_code = '5958' AND cp.calculation_code  = '200' AND cp.unit_code='200' AND cp.industry_branch_code IS NOT NULL
+ORDER BY cp.industry_branch_code,cp.payroll_year, cp.payroll_quarter,cp.id  ; -- pozn.  - jeste jsem pridala ten unit code, aby TO bylo v korunach a ne tisicich osob
+-- ted uz je to DOUFEJME serazene spravne, pak jeste omrknout. ASi delaly problem ty ID
 
