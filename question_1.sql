2e28cd6f491f805961a2d3a83fc12181b06d3105
Tatana Dudackova <tanuldab@gmail.com>
2023-11-04 12:40:05 +0100

    prvni pokus - vyber sloupcu, ktere si vubec chci zobrazit, mozna se jeste rozhodnu jinak

--- question_1.sql
+++ question_1.sql
@@ -1,1 +1,7 @@
-pokus
+
+SELECT * 
+FROM t_tatana_dudackova_project_sql_primary_final;
+
+SELECT cp_id, payroll_value,industry_branch_code,payroll_year,payroll_quarter 
+FROM t_tatana_dudackova_project_sql_primary_final
+ORDER BY payroll_year; -- poznamka - zkusit prijoinovat znova rok a ctvrtleti
