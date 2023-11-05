c10e36b0cd53755a450694fa94eae7b24bb405d7
Tatana Dudackova <tanuldab@gmail.com>
2023-11-05 22:09:42 +0100

    poopravit poddotaz tak, aby vyhovoval pravidlum maria db

--- secondary_table_dudackova.sql
+++ secondary_table_dudackova.sql
@@ -1,2 +1,26 @@
-SELECT country 
+SELECT *
 FROM countries c;
+
+SELECT *
+FROM economies e;
+
+SELECT *
+FROM t_tatana_dudackova_project_sql_primary_final ttdpspf
+ORDER BY rok;
+
+SELECT *
+FROM t_tatana_dudackova_project_sql_primary_final ttdpspf
+ORDER BY rok DESC ;
+
+-- takze musim porovnavat roky 2006-2018
+
+SELECT c.country,e.`year` AS rok_secondary,e.gdp,e.gini,e.taxes
+FROM countries c
+JOIN economies e
+ON c.country =e.country AND c.continent = 'europe'
+WHERE e.`year` >= rok IN (SELECT DISTINCT rok FROM t_tatana_dudackova_project_sql_primary_final ORDER BY rok LIMIT 1)
+OR
+WHERE e.`year` <= rok IN (SELECT DISTINCT rok FROM t_tatana_dudackova_project_sql_primary_final ORDER BY rok DESC LIMIT 1);
+
+
+
