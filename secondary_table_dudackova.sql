d704be3b5b7bb4921fb342bff0541adfca1cbfaa
Tatana Dudackova <tanuldab@gmail.com>
2023-11-05 22:13:42 +0100

    pokusy

--- secondary_table_dudackova.sql
+++ secondary_table_dudackova.sql
@@ -18,9 +18,24 @@ SELECT c.country,e.`year` AS rok_secondary,e.gdp,e.gini,e.taxes
 FROM countries c
 JOIN economies e
 ON c.country =e.country AND c.continent = 'europe'
-WHERE e.`year` >= rok IN (SELECT DISTINCT rok FROM t_tatana_dudackova_project_sql_primary_final ORDER BY rok LIMIT 1)
+WHERE e.`year` >= rok IN (SELECT  rok FROM t_tatana_dudackova_project_sql_primary_final ORDER BY rok LIMIT 1)
 OR
-WHERE e.`year` <= rok IN (SELECT DISTINCT rok FROM t_tatana_dudackova_project_sql_primary_final ORDER BY rok DESC LIMIT 1);
+WHERE e.`year` <= rok IN (SELECT  rok FROM t_tatana_dudackova_project_sql_primary_final ORDER BY rok DESC LIMIT 1);
 
 
+SELECT c.country,e.`year` AS rok_secondary,e.gdp,e.gini,e.taxes
+FROM countries c
+JOIN economies e
+ON c.country =e.country AND c.continent = 'europe'
+WHERE e.`year` >= rok IN (SELECT  rok FROM t_tatana_dudackova_project_sql_primary_final ORDER BY rok LIMIT 1)
+OR
+WHERE e.`year` <= rok IN (SELECT  rok FROM t_tatana_dudackova_project_sql_primary_final ORDER BY rok DESC LIMIT 1);
+
+
+SELECT min(rok) FROM t_tatana_dudackova_project_sql_primary_final;
+SELECT max(rok) FROM t_tatana_dudackova_project_sql_primary_final;
+
+
+SELECT * FROM Products
+WHERE Price BETWEEN 10 AND 20;
 
