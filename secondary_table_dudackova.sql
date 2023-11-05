747ca245a8ce46a8d44fc787fdf95eaf8af28b84
Tatana Dudackova <tanuldab@gmail.com>
2023-11-05 22:24:27 +0100

    secondary table, snad konecna verze

--- secondary_table_dudackova.sql
+++ secondary_table_dudackova.sql
@@ -1,41 +1,11 @@
-SELECT *
-FROM countries c;
-
-SELECT *
-FROM economies e;
-
-SELECT *
-FROM t_tatana_dudackova_project_sql_primary_final ttdpspf
-ORDER BY rok;
-
-SELECT *
-FROM t_tatana_dudackova_project_sql_primary_final ttdpspf
-ORDER BY rok DESC ;
-
--- takze musim porovnavat roky 2006-2018
-
-SELECT c.country,e.`year` AS rok_secondary,e.gdp,e.gini,e.taxes
+CREATE TABLE t_tatana_dudackova_project_sql_secondary_final AS (
+SELECT c.country,c.population,e.`year` AS `year`,e.gdp,e.gini,e.taxes
 FROM countries c
 JOIN economies e
 ON c.country =e.country AND c.continent = 'europe'
-WHERE e.`year` >= rok IN (SELECT  rok FROM t_tatana_dudackova_project_sql_primary_final ORDER BY rok LIMIT 1)
-OR
-WHERE e.`year` <= rok IN (SELECT  rok FROM t_tatana_dudackova_project_sql_primary_final ORDER BY rok DESC LIMIT 1);
+WHERE e.`year` BETWEEN 
+	(SELECT min(rok) FROM t_tatana_dudackova_project_sql_primary_final) AND 
+	(SELECT max(rok) FROM t_tatana_dudackova_project_sql_primary_final));
 
-
-SELECT c.country,e.`year` AS rok_secondary,e.gdp,e.gini,e.taxes
-FROM countries c
-JOIN economies e
-ON c.country =e.country AND c.continent = 'europe'
-WHERE e.`year` >= rok IN (SELECT  rok FROM t_tatana_dudackova_project_sql_primary_final ORDER BY rok LIMIT 1)
-OR
-WHERE e.`year` <= rok IN (SELECT  rok FROM t_tatana_dudackova_project_sql_primary_final ORDER BY rok DESC LIMIT 1);
-
-
-SELECT min(rok) FROM t_tatana_dudackova_project_sql_primary_final;
-SELECT max(rok) FROM t_tatana_dudackova_project_sql_primary_final;
-
-
-SELECT * FROM Products
-WHERE Price BETWEEN 10 AND 20;
+-- vyber roku jsem puvodne chtela udelat pomoci in, vnoreneho order by a limit, ale vratila se mi chybova hlaska, ze tato verze maria DB zvolenou kombinaci prikazu zatim nepodporuje
 
