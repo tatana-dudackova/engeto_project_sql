982fa2bee731c5ba1bdf48c4abe0ca52d55ef3f3
Tatana Dudackova <tanuldab@gmail.com>
2023-11-05 20:43:10 +0100

    drobna uprava

--- query_1new.sql
+++ query_1new.sql
@@ -13,5 +13,5 @@ FROM t_tatana_dudackova_project_sql_primary_final
 ORDER BY kod_odvetvi, rok,ctvrtleti, mzdy_id;
 
 -- jeste omrknout/zkontrolovat, proc sloupec mezd za predchozi rok neni na zacatku vzdycky nul, je divny, ze to asi odnekud bere ty hodnoty
--- podle toho sloupce zjistuji, ze nektere nekdy doslo k mezirocnimu poklesu
+-- podle toho sloupce zjistuji, ze nektere nekdy doslo k mezirocnimu poklesu 
 
