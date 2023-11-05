32696e4f0c7cf4599d23ecd8b33128525a1a4a13
Tatana Dudackova <tanuldab@gmail.com>
2023-11-05 21:46:18 +0100

    zacatek 5. ukolu, jeste dodelat

--- query_5.sql
+++ query_5.sql
@@ -1,2 +1,15 @@
-SELECT *,yearly_average_temperature 
-FROM countries c;
+SELECT *
+FROM t_tatana_dudackova_project_sql_primary_final ttdpspf;
+
+-- Definuji si, ze vyrazny narust HDP bude vice nez 5 % 
+SELECT DISTINCT 
+rok,((GDP/gdp_prev_year*100)-100) AS zmena_hdp,
+CASE 
+	WHEN ((GDP/gdp_prev_year*100)-100) > 5 THEN 'vyrazny narust hdp'
+	ELSE 'nevyrazny narust'
+END AS zhodnoceni_zmeny_hdp
+FROM t_tatana_dudackova_project_sql_primary_final ttdpspf;
+
+-- poznamka: jeste porovnat s temi mzdami a cenami, slo by pouzit zavery predchoziho ukolu - DODELAT!!!
+
+
