49bc18548f8c1b95f374906264f8a863b407592c
Tatana Dudackova <tanuldab@gmail.com>
2023-11-09 18:52:47 +0100

    uprava prikazu

--- query_5.sql
+++ query_5.sql
@@ -12,7 +12,7 @@ FROM t_tatana_dudackova_project_sql_primary_final ttdpspf
 ORDER BY rok DESC ;
 -- pozn - overila jsem si na webu csu, ze je ten vypocet vyse dobre
 
--- Dalsi podminky - mzdy a ceny
+-- Dalsi podminky - mzdy a ceny - hranice 10 procent
 SELECT DISTINCT 
 rok,((GDP/gdp_prev_year*100)-100) AS zmena_hdp,
 CASE 
@@ -30,6 +30,30 @@ END AS 'posouzeni vlivu'
 FROM t_tatana_dudackova_project_sql_primary_final ttdpspf
 GROUP BY rok 
 ORDER BY rok ;
+
+
+-- Dalsi podminky - mzdy a ceny - hranice 5 procent
+SELECT DISTINCT 
+rok,((GDP/gdp_prev_year*100)-100) AS zmena_hdp,
+CASE 
+	WHEN ((GDP/gdp_prev_year*100)-100) > 5 THEN 'vyrazny narust hdp'
+	ELSE 'nevyrazny narust nebo pokles'
+END AS zhodnoceni_zmeny_hdp,
+avg(vyse_mezd)/avg(vyse_mezd_prev_year)*100-100 AS mzdy_mezirocne,
+avg(vyse_cen)/avg(vyse_cen_prev_year)*100-100 AS ceny_mezirocne,
+CASE
+	WHEN (((GDP/gdp_prev_year*100)-100) > 5) AND (avg(vyse_mezd)/avg(vyse_mezd_prev_year)*100-100 > 5 OR  avg(vyse_cen)/avg(vyse_cen_prev_year)*100-100 > 5) THEN 'vlivem vyrazneho rustu hdp doslo k vyraznemu rustu mezd a/nebo cen'
+	WHEN (((GDP/gdp_prev_year*100)-100) > 5) AND (avg(vyse_mezd)/avg(vyse_mezd_prev_year)*100-100 < 5 OR  avg(vyse_cen)/avg(vyse_cen_prev_year)*100-100 < 5) THEN 'nastal vyrazny rust hdp bez dalsich vlivu'
+	WHEN (((GDP/gdp_prev_year*100)-100) < 5) THEN 'nevyrazny rust hdp'
+	ELSE 'jina moznost'
+END AS 'posouzeni vlivu'
+FROM t_tatana_dudackova_project_sql_primary_final ttdpspf
+GROUP BY rok 
+ORDER BY rok ;
+
+
+
+
 
 
 
