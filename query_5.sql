fb02cb651d8f94f127a9f457fe03b642571d54a6
Tatana Dudackova <tanuldab@gmail.com>
2023-11-09 19:09:43 +0100

    snad uz finalni vyzkumna otazka 5

--- query_5.sql
+++ query_5.sql
@@ -1,17 +1,5 @@
-SELECT *
-FROM t_tatana_dudackova_project_sql_primary_final ttdpspf;
 
--- Definuji si, ze vyrazny narust HDP bude vice nez 5 % 
-SELECT DISTINCT 
-rok,((GDP/gdp_prev_year*100)-100) AS zmena_hdp,
-CASE 
-	WHEN ((GDP/gdp_prev_year*100)-100) > 5 THEN 'vyrazny narust hdp'
-	ELSE 'nevyrazny narust nebo pokles'
-END AS zhodnoceni_zmeny_hdp
-FROM t_tatana_dudackova_project_sql_primary_final ttdpspf
-ORDER BY rok DESC ;
--- pozn - overila jsem si na webu csu, ze je ten vypocet vyse dobre
-
+-- DOUFEJME, ze uz finalni dotaz
 
 SELECT DISTINCT 
 	rok,
