cbf8fb895e730f7babbbc496a12973f9305d8955
Tatana Dudackova <tanuldab@gmail.com>
2023-11-11 19:48:21 +0100

    drobna uprava

--- secondary_table_dudackova.sql
+++ secondary_table_dudackova.sql
@@ -1,3 +1,5 @@
+/* SEKUNDARNI TABULKA*/
+
 CREATE TABLE t_tatana_dudackova_project_sql_secondary_final AS (
 SELECT c.country,c.population,e.`year` AS `year`,e.gdp,e.gini,e.taxes
 FROM countries c
@@ -5,5 +7,5 @@ JOIN economies e
 ON c.country =e.country AND c.continent = 'europe'
 WHERE e.`year` BETWEEN 
 	(SELECT min(rok) FROM t_tatana_dudackova_project_sql_primary_final) AND 
-	(SELECT max(rok) FROM t_tatana_dudackova_project_sql_primary_final));
+	(SELECT max(rok) FROM t_tatana_dudackova_project_sql_primary_final));  
 
