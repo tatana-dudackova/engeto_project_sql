94126693d1f0efb1677b053e7d1690b193d11b55
Tatana Dudackova <tanuldab@gmail.com>
2023-11-11 20:16:16 +0100

    drobne upravy, zaokrouhleni nekterych hodnot

--- query_2.sql
+++ query_2.sql
@@ -1,21 +1,21 @@
-(SELECT avg(vyse_mezd) AS prumerne_ctvrtletni_mzdy_vsechna_odvetvi,avg(vyse_cen) AS prumerna_cena_ctvrtleti,nazev_zbozi,rok,ctvrtleti,round(avg(vyse_mezd)/avg(vyse_cen),2) AS vypocet,
+(SELECT round (avg(vyse_mezd)) AS prumerne_ctvrtletni_mzdy_vsechna_odvetvi,round(avg(vyse_cen),2) AS prumerna_cena_ctvrtleti,nazev_zbozi,rok,ctvrtleti,round(avg(vyse_mezd)/avg(vyse_cen),2) AS vypocet,
 CASE 
-	WHEN nazev_zbozi LIKE '%chléb%' THEN 'kg chleba'
+	WHEN nazev_zbozi LIKE '%chleb%' THEN 'kg chleba'
 	ELSE 'litru mleka'
 END AS jednotka
 FROM t_tatana_dudackova_project_sql_primary_final
-WHERE vyse_cen IS NOT NULL AND nazev_zbozi LIKE '%chléb%' OR nazev_zbozi LIKE '%mléko%'
+WHERE vyse_cen IS NOT NULL AND nazev_zbozi LIKE '%chleb%' OR nazev_zbozi LIKE '%mleko%'
 GROUP BY nazev_zbozi,rok,ctvrtleti
 ORDER BY rok,ctvrtleti
 LIMIT 2)
 UNION 
-(SELECT avg(vyse_mezd) AS prumerne_ctvrtletni_mzdy_vsechna_odvetvi,avg(vyse_cen) AS prumerna_cena_ctvrtleti,nazev_zbozi,rok,ctvrtleti,round(avg(vyse_mezd)/avg(vyse_cen),2) AS vypocet,
+(SELECT round(avg(vyse_mezd)) AS prumerne_ctvrtletni_mzdy_vsechna_odvetvi,round(avg(vyse_cen),2) AS prumerna_cena_ctvrtleti,nazev_zbozi,rok,ctvrtleti,round(avg(vyse_mezd)/avg(vyse_cen),2) AS vypocet,
 CASE 
-	WHEN nazev_zbozi LIKE '%chléb%' THEN 'kg chleba'
+	WHEN nazev_zbozi LIKE '%chleb%' THEN 'kg chleba'
 	ELSE 'litru mleka'
 END AS jednotka
 FROM t_tatana_dudackova_project_sql_primary_final
-WHERE vyse_cen IS NOT NULL AND nazev_zbozi LIKE '%chléb%' OR nazev_zbozi LIKE '%mléko%'
+WHERE vyse_cen IS NOT NULL AND nazev_zbozi LIKE '%chleb%' OR nazev_zbozi LIKE '%mleko%'
 GROUP BY nazev_zbozi,rok,ctvrtleti
 ORDER BY rok DESC ,ctvrtleti DESC
 LIMIT 2);
