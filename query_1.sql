d8663c0d45f221319fa36a60aa636f3b9c5efd4f
Tatana Dudackova <tanuldab@gmail.com>
2023-11-11 19:53:07 +0100

    drobna uprava

--- query_1.sql
+++ query_1.sql
@@ -1,68 +1,12 @@
-
-SELECT * 
+SELECT DISTINCT nazev_odvetvi,rok,ctvrtleti,vyse_mezd,vyse_mezd_prev_year,
+CASE 
+	WHEN vyse_mezd > vyse_mezd_prev_year THEN 'mzdy vzrostly'
+	WHEN vyse_mezd < vyse_mezd_prev_year THEN 'mzdy klesly'
+	WHEN vyse_mezd_prev_year IS NULL THEN 'chybi nam udaje'
+	ELSE 'mzdy zustaly stejne'
+END AS mezirocni_srovnani
 FROM t_tatana_dudackova_project_sql_primary_final
-ORDER BY industry_branch_code, payroll_year,payroll_quarter;
+ORDER BY mezirocni_srovnani, nazev_odvetvi, rok,ctvrtleti;
 
-SELECT DISTINCT cp.id,cp2.id, cp.value,cp2.value AS druha_tabulka_value,cp.industry_branch_code, cp2.industry_branch_code, cp.payroll_year,cp.payroll_quarter,cp2.payroll_year +1 AS payroll_year2,cp2.payroll_quarter AS payroll_quarter2
-FROM czechia_payroll cp
-LEFT JOIN czechia_payroll cp2 
-ON cp.value_type_code = cp2.value_type_code
-AND cp.unit_code = cp2.unit_code 
-AND cp.calculation_code = cp2.calculation_code
-AND cp.industry_branch_code = cp2.industry_branch_code 
-AND cp.payroll_year = cp2.payroll_year +1
-AND cp.payroll_quarter = cp2.payroll_quarter 
-WHERE cp.value_type_code = '5958' AND cp.calculation_code  = '200'
-ORDER BY cp.id,cp2.id; -- jooo, ted se mi TO posunulo spravne. zkontrolovat ale odvetvi.
-   
--- poznamka: posun mozna zkusit udelat uz rovnou v te finalni tabulce. Budu tam potrebovat udelat value2 a payroll_year2. 
--- v tom sql dotazu bych pak uz udelala jen case
--- jinak se mi to asi bude pocitat priserne dlouho
-
--- --------------------
--- tady jeste zkusim udelat CASE - funguje to
--- pak jeste bude asi zapotrebi vybrat to prislusne odvetvi ???
-
-
-SELECT DISTINCT cp.id,cp2.id, cp.value,cp2.value AS druha_tabulka_value,cp.industry_branch_code, cp2.industry_branch_code, cp.payroll_year,cp.payroll_quarter,cp2.payroll_year +1 AS payroll_year2,cp2.payroll_quarter AS payroll_quarter2,
-CASE 
-	WHEN cp.value>cp2.value THEN 'mzdy vzrostly'
-	WHEN cp2.value>cp.value THEN 'mzdy klesly'
-	WHEN cp2.value IS NULL THEN 'chybi nam udaje'
-	ELSE 'mzdy zustaly stejne'
-END AS porovnani
-FROM czechia_payroll cp
-LEFT JOIN czechia_payroll cp2 
-ON cp.value_type_code = cp2.value_type_code
-AND cp.unit_code = cp2.unit_code 
-AND cp.calculation_code = cp2.calculation_code
-AND cp.industry_branch_code = cp2.industry_branch_code 
-AND cp.payroll_year = cp2.payroll_year +1
-AND cp.payroll_quarter = cp2.payroll_quarter 
-WHERE cp.value_type_code = '5958' AND cp.calculation_code  = '200' AND cp.unit_code='200' -- pozn.  - jeste jsem pridala ten unit code, aby TO bylo v korunach a ne tisicich osob
-ORDER BY cp.id,cp2.id;
--- prikaz viz vyse zkusit udelat pak podle te finalni tabulky, pokusit se ale mit v te finalni tabulce uz prijoinovany ten spravny sloupec
--- pozn. - pozor, mam tam chybu :( - od nejakeho radku 1053, nebo tak :( asi kvuli idickum, to pak take napsat do popisu
--- --------------------
-
--- pokus o opravu
-
-SELECT DISTINCT cp.id,cp2.id, cp.value,cp2.value AS druha_tabulka_value,cp.industry_branch_code, cp2.industry_branch_code, cp.payroll_year,cp.payroll_quarter,cp2.payroll_year +1 AS payroll_year2,cp2.payroll_quarter AS payroll_quarter2,
-CASE 
-	WHEN cp.value>cp2.value THEN 'mzdy vzrostly'
-	WHEN cp2.value>cp.value THEN 'mzdy klesly'
-	WHEN cp2.value IS NULL THEN 'chybi nam udaje'
-	ELSE 'mzdy zustaly stejne'
-END AS porovnani
-FROM czechia_payroll cp
-LEFT JOIN czechia_payroll cp2 
-ON cp.value_type_code = cp2.value_type_code
-AND cp.unit_code = cp2.unit_code 
-AND cp.calculation_code = cp2.calculation_code
-AND cp.industry_branch_code = cp2.industry_branch_code 
-AND cp.payroll_year = cp2.payroll_year +1
-AND cp.payroll_quarter = cp2.payroll_quarter 
-WHERE cp.value_type_code = '5958' AND cp.calculation_code  = '200' AND cp.unit_code='200' AND cp.industry_branch_code IS NOT NULL
-ORDER BY cp.industry_branch_code,cp.payroll_year, cp.payroll_quarter,cp.id  ; -- pozn.  - jeste jsem pridala ten unit code, aby TO bylo v korunach a ne tisicich osob
--- ted uz je to DOUFEJME serazene spravne, pak jeste omrknout. ASi delaly problem ty ID
+-- podle sloupce mezirocni_srovnani zjistuji, ze nekdy doslo k mezirocnimu poklesu, takze neni pravda, ze by mzdy porad jen rostly
 
